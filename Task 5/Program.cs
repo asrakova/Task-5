25c3836423cfa702f255b97a54b342eeafd9ecc5
Анна <@LAPTOP-63K8I5TL>
2017-09-06 21:06:45 +0500

    Проверка ввода

--- Task 5/Program.cs
+++ Task 5/Program.cs
@@ -8,34 +8,65 @@ namespace Task_5
 {
     class Program
     {
-
+        /// <summary>
+        /// Проверка ввода натуральных чисел
+        /// </summary>
+        /// <returns>Натуральное число</returns>
+        public static int ReadPositive()
+        {
+            int k = 0; bool ok;
+            do
+            {
+                ok = int.TryParse(Console.ReadLine(), out k);
+                if (!ok || k <= 0) Console.WriteLine("Неправильный ввод. Ожидалось натуральное число. Пожалуйста, повторите ввод");
+            }
+            while (!ok || k <= 0);
+            return k;
+        }
 
 
         static void Main(string[] args)
         {
             // Вводим значение N
-            int N = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите размерность матрицы");
+            int N = ReadPositive();
             int[,] matrix = new int[N, N];      // Матрица n-ого порядка 
             char[] ch = { ' ' };
-            // Считываем значение матрицы по строкам и запоминаем
+            // Считываем значение матрицы по строкам и запоминаем (с проверкой ввода)
             for (int i = 0; i < N; i++)
             {
-                string[] str = Console.ReadLine().Split(ch, StringSplitOptions.RemoveEmptyEntries);
-                for (int j = 0; j < str.Length; j++)
-                    
-                    matrix[i, j] = Int16.Parse(str[j]);
+                bool ok = false;
+                Console.WriteLine("Введите строку матрицы через пробел");
+                do
+                {
+                    string[] str = Console.ReadLine().Split(ch, StringSplitOptions.RemoveEmptyEntries);
+                    if (str.Length >= N)
+                        for (int j = 0; j < N; j++)
+                        {
+                            ok = int.TryParse(str[j], out matrix[i, j]);
+                            if (!ok)
+                            {
+                                Console.WriteLine("Ошибка ввода. Введите строку заново");
+                                break;
+                            }
+                        }
+                    else Console.WriteLine("Недостаточно элементов");
+                } while (!ok);
             }
 
+            // Составляем ряд минимальных элементов
             int[] b = new int[N];
-            b[0] = 1;
-            for (int i = 1; i< N; i++)
+            b[0] = 1;               // В 1ой строке нет элементов до главной диагонали
+            // Последовательно находим минимальные элементы
+            for (int i = 1; i < N; i++)
             {
                 int min = matrix[i, 0];
                 for (int j = 1; j < i; j++)
                     if (matrix[i, j] < min) min = matrix[i, j];
                 b[i] = min;
             }
-
+            // Ввывод последовательности
+            Console.WriteLine("Полученный ряд:");
             for (int i = 0; i < N; i++)
                 Console.Write("{0} ", b[i]);
             Console.ReadLine();
